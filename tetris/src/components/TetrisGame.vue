a0cdfd1e62db9035967c0c731fb90cbacf595705
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-13 20:01:50 +0100

    Initial folders

---
+++ tetris/src/components/TetrisGame.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>I am Tetris Game</h2>
+</template>
---
+++ tetris/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TetrisGame from '@/components/TetrisGame.vue';
+
+
+</script>
+
+<template>
+  <main>
+    <TetrisGame />
+  </main>
+</template>
