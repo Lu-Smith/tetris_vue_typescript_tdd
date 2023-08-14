7c06d0ed3081fd1d12cec06263a0a815c1384232
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-14 22:55:59 +0100

    Creating testing for App

---
+++ tetris/src/components/__tests__/App.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import AppVue from '@/App.vue'
+
+
+describe('App', () => {
+  it('renders all elements correctly', () => {
+    const wrapper = mount(AppVue)
+    
+})
+})
