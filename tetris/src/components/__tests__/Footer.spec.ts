3941a654450d0797202bc2212562dc8e4d29efd3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-14 22:51:49 +0100

    Updated Footer testing

--- tetris/src/components/__tests__/Footer.spec.ts
+++ tetris/src/components/__tests__/Footer.spec.ts
@@ -16,9 +16,11 @@ describe('Footer', () => {
     const profileLink = footerElement.find('a.profile-link')
     expect(profileLink.exists()).toBe(true)
     expect(profileLink.attributes('href')).toContain('https://www.lunasmithart.com/')
+    expect(profileLink.attributes('target')).toBe('_blank')
 
     const gitHubLink = footerElement.find('a.gitHub-link')
     expect(gitHubLink.exists()).toBe(true)
     expect(gitHubLink.attributes('href')).toContain('https://github.com/Lu-Smith/tetris_vue_typescript_tdd')
-  })
+    expect(gitHubLink.attributes('target')).toBe('_blank')
+})
 })
