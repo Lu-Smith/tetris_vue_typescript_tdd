50ed5ac9af6d730bdc4c361bb49ba980a9e7dc73
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-14 22:48:04 +0100

    Updated Footer testing

--- tetris/src/components/__tests__/Footer.spec.ts
+++ tetris/src/components/__tests__/Footer.spec.ts
@@ -8,5 +8,14 @@ describe('Footer', () => {
   it('renders all elements correctly', () => {
     const wrapper = mount(FooterComponentVue)
     expect(wrapper.text()).toContain('This game was coded by Luna Smith and is available as an open-source project on GitHub.')
+    
+    const footerElement = wrapper.find('h4')
+    expect(footerElement.exists()).toBeTruthy
+
+    const profileLink = footerElement.find('a.profile-link')
+    expect(profileLink.exists()).toBe(true)
+
+     const gitHubLink = footerElement.find('a.gitHub-link')
+    expect(gitHubLink.exists()).toBeFalsy
   })
 })
