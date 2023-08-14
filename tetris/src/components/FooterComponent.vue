c2e1af103fab00ed86cd08c33b869c8285a11a47
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-14 22:33:36 +0100

    Created FooterComponent

---
+++ tetris/src/components/FooterComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="Footer">
+      <h4>This game was coded by 
+        <a class="profile-link" href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a> 
+        and is available as an open-source project on 
+        <a class="gitHub-link" href="https://github.com/Lu-Smith/tetris_vue_typescript_tdd" target="_blank">GitHub</a>.
+      </h4>
+    </div>
+  </template>
