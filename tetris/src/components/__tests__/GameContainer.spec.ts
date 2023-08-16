0defb0b61bdc911fbdb64a7ef8019ba90f29e35e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-16 19:09:29 +0100

    Created testing for GameContainer

---
+++ tetris/src/components/__tests__/GameContainer.spec.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest";
+import { mount } from '@vue/test-utils';
+import GameContainerVue from "../GameContainer.vue";
+
+describe('GameContainer', () => {
+    it('renders all elements correctly', () => {
+        const wrapper = mount(GameContainerVue)
+        //display board
+        const boardContainer = wrapper.find('div.board-container')
+        expect(boardContainer.exists()).toBe(true)
+    })
+})
