6591831947149a0af645db54c8600a9afae53278
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-16 19:11:52 +0100

    Updated testing in GameContainer

--- tetris/src/components/__tests__/GameContainer.spec.ts
+++ tetris/src/components/__tests__/GameContainer.spec.ts
@@ -5,8 +5,11 @@ import GameContainerVue from "../GameContainer.vue";
 describe('GameContainer', () => {
     it('renders all elements correctly', () => {
         const wrapper = mount(GameContainerVue)
+
         //display board
         const boardContainer = wrapper.find('div.board-container')
         expect(boardContainer.exists()).toBe(true)
+        const boardGrid = wrapper.find('grid.board')
+        expect(boardGrid.exists()).toBe(true)
     })
 })
