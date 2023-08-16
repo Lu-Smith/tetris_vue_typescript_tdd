c6296c240c80ebf65cfe1a788dfc4a648305754a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-16 19:28:07 +0100

    Updated GameContainer

--- tetris/src/components/GameContainer.vue
+++ tetris/src/components/GameContainer.vue
@@ -1,6 +1,16 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+const rows = ref(Array.from({length: 18}, () => Array(10).fill(0)))
+</script>
+
 <template>
     <div class="board-container">
         <div class="board">
+            <div class="rows" v-for="(row, rowIndex) in rows" :key="rowIndex">
+                <div class="cell" v-for="(cell, cellIndex) in row" :key="cellIndex">
+                    {{ cell }}
+                </div>
+            </div>
 
         </div>
     </div>
