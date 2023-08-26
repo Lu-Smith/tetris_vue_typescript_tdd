0b15eddbba4510c69c6d075d22d4432ac312ef4e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-26 18:11:48 +0100

    Updated GameContainer logic

--- tetris/src/components/GameContainer.vue
+++ tetris/src/components/GameContainer.vue
@@ -46,17 +46,21 @@ watch(
         for (let col = 0; col < currentTetrisBlock.value[row].length; col++) {
           const blockValue = currentTetrisBlock.value[row][col];
           if (blockValue === 1) {
-            const newRow = row + 4 + right - left;
-            const newCol = col + seconds - 1 + down ;
+            let newRow = row + 4 + right - left;
+            let newCol = col + seconds - 1 + down ;
 
-            if (
-              newRow >= 0 &&
-              newRow < boardRows.value.length &&
-              newCol >= 0 &&
-              newCol < boardRows.value[0].length
-            ) {
+            if (newCol >= 0 ) {
+              if (newCol < boardRows.value[0].length) {
+                if (newRow < 0 ) {
+                  newRow = 0 + currentTetrisBlock.value.length;
+                } else if (newRow > boardRows.value.length ) {
+                  newRow = boardRows.value.length - currentTetrisBlock.value.length;
+                } 
+              } else {
+                newCol = boardRows.value[0].length - currentTetrisBlock.value[row].length;
+              }
               boardRows.value[newRow][newCol] = blockValue;
-            }
+            } 
           }
         }
       }
